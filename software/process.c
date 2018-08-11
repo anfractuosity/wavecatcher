cc9fb9175f6b1d49b02b532bd72a43dc2dd50df5
anfractuosity <anfractuosity@anfractuosity.com>
2018-08-11 21:14:55 +0100

    new process

--- software/process.c
+++ software/process.c
@@ -10,7 +10,7 @@ int countSetBits(unsigned int n) {
     return n & 0x0000003F;
 }
 
-int main() {
+int processA() {
 
     int i = 0;
 
@@ -33,4 +33,46 @@ int main() {
         fwrite(&tmp, 1, 1, o);
     }
 
+}
+
+int processB() {
+
+
+    FILE *o = fopen("out.bin", "ab");
+    FILE *f = fopen("dump.bin", "rb");
+
+    fseek(f, 0L, SEEK_END);
+    int sz = ftell(f);
+    rewind(f);
+
+    char *buf = malloc(sz);
+    fread(buf, 1, sz, f);
+    
+    int i = 0;
+
+    int mx = 100;
+
+    for(i=0;i<sz-mx;i+=mx/4){
+        int c = 0;
+        int max = mx / 4;
+        int sum = 0;
+
+        char *b = buf+i;
+        for (c = 0; c < max; c++) {
+            int val = ((int *)b)[c];
+            sum += countSetBits(val);
+        }
+
+        unsigned short tmp = sum*200;
+        printf("sum %d\n",tmp);
+        fwrite(&tmp, 2, 1, o);
+    }
+
+}
+
+
+int main(){
+
+    //processA();
+    processA();
 }
